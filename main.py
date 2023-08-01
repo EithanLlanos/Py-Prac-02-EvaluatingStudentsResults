3572beb9e757b45e7f8fb61b89cb3e63a2825dd7
EithanLlanos <eithanllanosb@gmail.com>
2023-08-01 08:22:26 -0500

    Base

---
+++ main.py
@@ -0,0 +1,12 @@
+class StudentsDataException(Exception):
+    pass
+
+
+class BadLine(StudentsDataException):
+    pass
+    # Write your code here.
+
+
+class FileEmpty(StudentsDataException):
+    pass
+    # Write your code here.
